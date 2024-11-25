0abf85c0259b624c07ac900cd166940a04ca72e3
artemdljnk <artemodessa31.01@gmail.com>
2024-11-25 13:34:48 +0200

    new_1.2

---
+++ 1/main.js
@@ -0,0 +1,46 @@
+    // let text1 = 'hello'
+    // let text2 = 'owu'
+    // let text3 = 'com'
+    // let text4 = 'ua'
+    //
+    // let num1 = 1
+    // let num2 = 10
+    // let num3 = -999
+    // let num4 = 123
+    // let num5 = 3.14
+    // let num6 = 2.7
+    // let num7 = 16
+    //
+    // let bool1 = false
+    // let bool2 = true
+    //
+    // console.log(text1, text2, text3, text4)
+    // console.log(num1, num2, num3, num4, num5, num6, num7)
+    // console.log(bool1, bool2)
+
+    // ------------------------------------------------------------------------
+
+    // let firsName = 'Artem'
+    // let middleName = 'Andreevich'
+    // let lastName = 'Dolzzhnenko'
+    //
+    // let person = `${lastName} ${firsName} ${middleName}`
+    // console.log(person)
+
+    // -----------------------------------------------------
+
+    // let a = 100;
+    // let b = '100';
+    // let c = true;
+    //
+    // console.log(typeof a, a)
+    // console.log(typeof b, b)
+    // console.log(typeof c, c)
+
+    // -------------------------------------------------------------------------------
+
+    // let firstName = prompt("What is your name?");
+    // let lastName = prompt("What is your last name?");
+    // let mmiddleName = prompt("What is your middle name?");
+    //
+    // console.log(`Your name is: ${mmiddleName} ${firstName} ${lastName}`);
