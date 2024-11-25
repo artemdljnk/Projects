2dd947742d7f4b8fe0bcf59b23ae989fcdc71038
artemdljnk <artemodessa31.01@gmail.com>
2024-11-25 17:55:42 +0200

    new_1.2

---
+++ 2/main.js
@@ -0,0 +1,103 @@
+// let qwerty = [
+//     'artem', 123, 321, 'nazar', 3.14, true, false, 9876, '123', -999
+// ]
+// console.log(qwerty)
+
+// ------------------------------------------------------
+
+// let book1 = {
+//     title: 'Aboba',
+//     pageCount: 16,
+//     genre: 'qwerty'
+// }
+// let book2 = {
+//     title: 'amogus',
+//     pageCount: 2020,
+//     genre: 'zxc'
+// }
+// let book3 = {
+//     title: 'hagivagi',
+//     pageCount: 2021,
+//     genre: 'boom'
+// }
+// console.log(book1)
+// console.log(book2)
+// console.log(book3)
+
+// ------------------------------------------------------------
+
+// let book1 = {
+//     title: 'Aboba',
+//     pageCount: 16,
+//     genre: 'qwerty'
+//     authors: [
+//         {
+//             name: 'AbobaJR',
+//             age: 52,
+//         }
+//     ]
+// }
+// let book2 = {
+//     title: 'amogus',
+//     pageCount: 2020,
+//     genre: 'zxc'
+//     authors: [
+//         {
+//             name: 'amogusJR',
+//             age: 42,
+//         }
+//     ]
+// }
+// let book3 = {
+//     title: 'hagivagi',
+//     pageCount: 2021,
+//     genre: 'boom',
+//     authors: [
+//         {
+//             name: 'hagivagiJR',
+//             age: 10442,
+//         }
+//     ]
+// }
+
+// -------------------------------------------------------------------------
+
+// let AllUsers = [
+//     { name: 'Oleg', username: 'oleg_pro', password: 'olegLove' },
+//     { name: 'artem', username: 'artem_pro', password: 'artemLove' },
+//     { name: 'nazar', username: 'nazar_pro', password: 'nazarLove' },
+//     { name: 'boba', username: 'boba_pro', password: 'bobaLove' },
+//     { name: 'biba', username: 'biba_pro', password: 'bibaLove' },
+//     { name: 'danya', username: 'danya_pro', password: 'danyaLove' },
+//     { name: 'dima', username: 'dima_pro', password: 'diamLove' },
+//     { name: 'denis', username: 'denis_pro', password: 'denisLove' },
+//     { name: 'radik', username: 'radik_pro', password: 'radikLove' },
+//     { name: 'valera', username: 'valera_pro', password: 'valeraLove' },
+// ]
+
+// console.log(
+//     AllUsers[0].password,
+//     AllUsers[1].password,
+//     AllUsers[2].password,
+//     AllUsers[3].password,
+//     AllUsers[4].password,
+//     AllUsers[5].password,
+//     AllUsers[6].password,
+//     AllUsers[7].password,
+//     AllUsers[8].password,
+//     AllUsers[9].password
+// )
+
+// -----------------------------------------------------------------------------------------------------------
+
+// let temperatureWeek = [
+//     { morning: 3, day: 20, evening: 18 },
+//     { morning: 16, day: 21, evening: 19 },
+//     { morning: 34, day: 22, evening: 20 },
+//     { morning: 33, day: 19, evening: 17 },
+//     { morning: 15, day: 8, evening: 21 },
+//     { morning: 12, day: 18, evening: 16 },
+//     { morning: 11, day: 17, evening: 15 },
+// ];
+
+// ------------------------------------------------------------------------
