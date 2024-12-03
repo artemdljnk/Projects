a902567e9f580fbdd1a5d3a8e24c097edcb9ae40
artemdljnk <artemodessa31.01@gmail.com>
2024-12-03 14:04:00 +0200

    new_1.5

---
+++ 4/main.js
@@ -0,0 +1,57 @@
+// function main(a, b) {
+// return a * b;
+// }
+
+// -----------------------------------------------------
+
+// function main(radius) {
+//     return Math.PI * radius * radius;
+// }
+
+// -------------------------------------------------
+
+// function main(height, radius) {
+//     return 2 * Math.PI * radius * height;
+//
+// }
+
+// ---------------------------------------------------
+
+// function main(massiv) {
+//     for (let massivElement of massiv.length) {
+//         console.log(massivElement);
+//     }
+// }
+
+// -------------------------------------
+
+// function main(text) {
+//     document.write(`<p>${text}</p>`);
+// }
+// main('Hello World');
+
+// -----------------------------------
+
+// function main(text) {
+//     document.write(`
+//                     <ul>
+//                         <li>${text}</li>
+//                         <li>${text}</li>
+//                         <li>${text}</li>
+//                     </ul>
+//                     `)
+//     main('QWERTY')
+// }
+
+// -----------------------------------------
+
+// function main(text, counter) {
+//     document.write(`<ul>`)
+//     for (let i = counter - 1; i >= 0; i--) {
+//         document.write(`<li>${text}</li>`);
+//     }
+//     document.write(`</ul>`)
+// }
+// main('Hello World!', 50);
+
+// ------------------------------------------------------
