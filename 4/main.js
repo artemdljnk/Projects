9bc4da5bc1154cf88e0d47b1cb9f1d80bef5b854
artemdljnk <artemodessa31.01@gmail.com>
2024-12-05 17:05:43 +0200

    new_1.6

---
+++ 4/main.js
@@ -0,0 +1,38 @@
+/*function main(a, b) {
+    let figura = a * b
+    console.log(figura)
+    return figura;
+}
+main(10, 10)*/
+
+// ----------------------------------------------------------------
+
+// function main(r) {
+//     let result = Math.PI * r * r
+//     console.log(result)
+//     return result
+// }
+// main(2)
+
+// --------------------------------------------------------------
+
+// function main(radius, height) {
+//     return 2 * Math.PI * radius * height
+// }
+// console.log(main(10, 10))
+
+// -------------------------------------------------------------
+
+// function exchange(sumUAH, currencyValues, ExchangeCurrency) {
+//     let chosenCurrency;
+//     for (let item of currencyValues) {
+//         if(item.currency === ExchangeCurrency) {
+//             chosenCurrency = item
+//         }
+//     }
+//     let result = sumUAH / chosenCurrency.value
+//     return result
+// }
+// console.log(exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}]))
+
+// -------------------------------------------------------------------------
